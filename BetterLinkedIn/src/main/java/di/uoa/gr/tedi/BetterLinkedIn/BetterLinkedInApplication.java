bd033ac18976262a661d7bbb2417ed6ee47365dd
roscar9 <38830836+roscar9@users.noreply.github.com>
2021-08-04 14:06:02 +0300

    Add files via upload

---
+++ BetterLinkedIn/src/main/java/di/uoa/gr/tedi/BetterLinkedIn/BetterLinkedInApplication.java
@@ -0,0 +1,13 @@
+package di.uoa.gr.tedi.BetterLinkedIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterLinkedInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterLinkedInApplication.class, args);
+	}
+
+}
---
+++ BetterLinkedIn/src/test/java/di/uoa/gr/tedi/BetterLinkedIn/BetterLinkedInApplicationTests.java
@@ -0,0 +1,13 @@
+package di.uoa.gr.tedi.BetterLinkedIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterLinkedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
