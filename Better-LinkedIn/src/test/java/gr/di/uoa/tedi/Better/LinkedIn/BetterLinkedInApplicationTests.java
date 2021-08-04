9df365d25578238396940921691ee129cbe809bb
roscar9 <38830836+roscar9@users.noreply.github.com>
2021-08-04 13:40:33 +0300

    Add files via upload

---
+++ Better-LinkedIn/src/test/java/gr/di/uoa/tedi/Better/LinkedIn/BetterLinkedInApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.di.uoa.tedi.Better.LinkedIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterLinkedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
